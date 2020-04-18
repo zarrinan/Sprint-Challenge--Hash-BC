5d83ee2aff997cb7df3df658eeee09d8c3089243
Niyazova <zniyazov@NYMIT058770.local>
2020-04-18 13:20:01 -0400

    Add blockchain solution

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -26,6 +26,12 @@ def proof_of_work(last_proof):
     proof = 0
     #  TODO: Your code here
 
+    pr_last_proof = f'{last_proof}'.encode()
+    last_hash = hashlib.sha256(pr_last_proof).hexdigest()
+
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
+
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
 
@@ -40,7 +46,9 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    pr_guess = f'{proof}'.encode()
+    guess_hash = hashlib.sha256(pr_guess).hexdigest()
+    return last_hash[-5:] == guess_hash[:5]
 
 
 if __name__ == '__main__':
@@ -66,6 +74,7 @@ if __name__ == '__main__':
         # Get the last proof from the server
         r = requests.get(url=node + "/last_proof")
         data = r.json()
+        print(data)
         new_proof = proof_of_work(data.get('proof'))
 
         post_data = {"proof": new_proof,
